20a0eadc373c6e311f7429a3a983dad2338891bb
Marek Pizner <m.pizner@gmail.com>
2014-09-21 11:54:22 +0100

    js

--- scripts.js
+++ scripts.js
@@ -34,6 +34,8 @@ var img7;
 var img8;
 
 var Count = 0;
+var maxCards=27;
+
 
 function Card(x)
 {
@@ -46,8 +48,37 @@ function Card(x)
   document.getElementById('card6').disabled = true;
   document.getElementById('card7').disabled = true;
   document.getElementById('card8').disabled = true;
+  switch (x)
+  {
+    case 1:
+          var number = img1;
+          break;
+    case 2:
+          var number = img2;
+          break;
+    case 3:
+          var number = img3;
+          break;
+    case 4:
+          var number = img4;
+          break;
+    case 5:
+          var number = img5;
+          break;
+    case 6:
+          var number = img6;
+          break;
+    case 7:
+          var number = img7;
+          break;
+    case 8:
+          var number = img8;
+          break;
 
-  var xml = "xml/" + x + ".xml";
+  }
+
+  var xml = "xml/" + number + ".xml";
+
   if(window.XMLHttpRequest)
     {
       xhttp = new XMLHttpRequest();
@@ -62,13 +93,14 @@ function Card(x)
     xmlDoc=xhttp.responseXML;
 
 
+
   var BuildersRR = xmlDoc.getElementsByTagName("BuildersR")[0].childNodes[0].nodeValue;
   var BricksRR = xmlDoc.getElementsByTagName("BricksR")[0].childNodes[0].nodeValue;
   var SoldiersRR = xmlDoc.getElementsByTagName("SoldiersR")[0].childNodes[0].nodeValue;
   var WeaponsRR = xmlDoc.getElementsByTagName("WeaponsR")[0].childNodes[0].nodeValue;
   var WizardsRR = xmlDoc.getElementsByTagName("WizardsR")[0].childNodes[0].nodeValue;
   var CrystalsRR = xmlDoc.getElementsByTagName("CrystalsR")[0].childNodes[0].nodeValue;
-  alert(BuildersRR + " :: " + BricksRR + " :: " + SoldiersRR + " :: " + WeaponsRR );
+
   var BuildersAdd = xmlDoc.getElementsByTagName("BuildersAdd")[0].childNodes[0].nodeValue;
   var BricksAdd = xmlDoc.getElementsByTagName("BricksAdd")[0].childNodes[0].nodeValue;
   var SoldiersAdd = xmlDoc.getElementsByTagName("SoldiersAdd")[0].childNodes[0].nodeValue;
@@ -99,30 +131,76 @@ function Card(x)
       CrystalsB = Number(CrystalsB) - Number(CrystalsRR);
 
       BuildersB = Number(BuildersB) + Number(BuildersAdd);
+      if(BuildersB < 0){
+        BuildersB = 0;
+      }
       BricksB = Number(BricksB) + Number(BricksAdd);
+      if(BricksB < 0){
+        BricksB = 0;
+      }
       SoldiersB = Number(SoldiersB) + Number(SoldiersAdd);
+      if(SoldiersB < 0){
+        SoldiersB = 0;
+      }
       WeaponsB = Number(WeaponsB) + Number(WeaponsAdd);
+      if(WeaponsB < 0){
+        WeaponsB = 0;
+      }
       WizardsB = Number(WizardsB) + Number(WizardsAdd);
+      if(WizardsB < 0){
+        WizardsB = 0;
+      }
       CrystalsB = Number(CrystalsB) + Number(CrystalsAdd);
+      if(CrystalsB < 0){
+        CrystalsB = 0;
+      }
       CastleB = Number(CastleB) + Number(CastleAdd);
+      if(CastleB < 0){
+        CastleB = 0;
+      }
       WallsB = Number(WallsB) + Number(WallsAdd);
+      if(WallsB < 0){
+        WallsB = 0;
+      }
 
       BuildersR = Number(BuildersR) + Number(BuildersE);
+      if(BuildersR < 0){
+        BuildersR = 0;
+      }
       BricksR = Number(BricksR) + Number(BricksE);
+      if(BricksR < 0){
+        BricksR = 0;
+      }
       SoldiersR = Number(SoldiersR) + Number(SoldiersE);
+      if(SoldiersR < 0){
+        SoldiersR = 0;
+      }
       WeaponsR = Number(WeaponsR) + Number(WeaponsE);
+      if(WeaponsR < 0){
+        WeaponsR = 0;
+      }
       WizardsR = Number(WizardsR) + Number(WizardsE);
+      if(WizardsR < 0){
+        WizardsR = 0;
+      }
       CrystalsR = Number(CrystalsR) + Number(CrystalsE);
+      if(CrystalsR < 0){
+        CrystalsR = 0;
+      }
       CastelR = Number(CastleR) + Number(CastleE);
+      if(CastleR < 0){
+        CastleR = 0;
+      }
       WallsR = Number(WallsR) + Number(WallsE);
+      if(WallsR < 0){
+        WallsR = 0;
+      }
 
       WallsR = Number(WallsR) - Number(AttackE);
       if(Number(WallsR) < 0){
         CastleR = Number(CastleR) - Number(WallstR);
         Number(WallsR) = 0;
       }
-    }else{
-      alert("Not enought supplays Black\n BricksRR: "+BricksRR+ " BricksR: " + BricksB +"\nWeaponsRR: "+WeaponsRR + " WeaponsB: " + WeaponsB +"\nCrystalsRR: " + CrystalsRR + " CrystalsB: " + CrystalsB );
     }
 
   }else{
@@ -134,30 +212,76 @@ function Card(x)
           CrystalsR = Number (CrystalsR) - Number (CrystalsRR);
 
           BuildersR = Number(BuildersR) + Number(BuildersAdd);
+          if(BuildersR < 0){
+            BuildersR = 0;
+          }
           BricksR = Number(BricksR) + Number(BricksAdd);
+          if(BricksR < 0){
+            BricksR = 0;
+          }
           SoldiersR = Number(SoldiersR) + Number(SoldiersAdd);
+          if(SoldiersR < 0){
+            SoldiersR = 0;
+          }
           WeaponsR = Number(WeaponsR) + Number(WeaponsAdd);
+          if(WeaponsR < 0){
+            WeaponsR = 0;
+          }
           WizardsR = Number(WizardsR) + Number(WizardsAdd);
+          if(WizardsR < 0){
+            WizardsR = 0;
+          }
           CrystalsR = Number(CrystalsR) + Number(CrystalsAdd);
+          if(CrystalsR < 0){
+            CrystalsR = 0;
+          }
           CastleR = Number(CastleR) + Number(CastleAdd);
+          if(CastleR < 0){
+            CastleR = 0;
+          }
           WallsR = Number(WallsR) + Number(WallsAdd);
+          if(WallsR < 0){
+            WallsR = 0;
+          }
 
           BuildersB = Number(BuildersB) + Number(BuildersE);
+          if(BuildersB < 0){
+            BuildersB = 0;
+          }
           BricksB = Number(BricksB) + Number(BricksE);
+          if(BricksB < 0){
+            BricksB = 0;
+          }
           SoldiersB = Number(SoldiersB) + Number(SoldiersE);
+          if(SoldiersB < 0){
+            SoldiersB = 0;
+          }
           WeaponsB = Number(WeaponsB) + Number(WeaponsE);
+          if(WeaponsB < 0){
+            WeaponsB = 0;
+          }
           WizardsB = Number(WizardsB) + Number(WizardsE);
+          if(WizardsB < 0){
+            WizardsB = 0;
+          }
           CrystalsB = Number(CrystalsB) + Number(CrystalsE);
+          if(CrystalsB < 0){
+            CrystalsB = 0;
+          }
           CastleB = Number(CastleB) + Number(CastleE);
+          if(CastleB < 0){
+            CastleB = 0;
+          }
           WallsB = Number(WallsB) + Number(WallsE);
+          if(WallsB < 0){
+            WallsB = 0;
+          }
 
           WallsB = Number(WallsB) - Number(AttackE);
           if(Number(WallsB) < 0){
             CastleB = Number(CastleB) - Number(WallstR);
             Number(WallsR) = 0;
           }
-        }else{
-          alert("Not enought supplays Red\n BricksRR: "+BricksRR+ " BricksR: " + BricksR +"\nWeaponsRR: "+WeaponsRR + " WeaponsR: " + WeaponsR +"\nCrystalsRR: " + CrystalsRR + " CrystalsR: " + CrystalsR );
         }
       }
 
@@ -171,14 +295,14 @@ function Card(x)
 
 function RandomPicture()
 {
-  img1 = Math.floor((Math.random() * 27)+1);
-  img2 = Math.floor((Math.random() * 27)+1);
-  img3 = Math.floor((Math.random() * 27)+1);
-  img4 = Math.floor((Math.random() * 27)+1);
-  img5 = Math.floor((Math.random() * 27)+1);
-  img6 = Math.floor((Math.random() * 27)+1);
-  img7 = Math.floor((Math.random() * 27)+1);
-  img8 = Math.floor((Math.random() * 27)+1);
+  img1 = Math.floor((Math.random() * maxCards)+1);
+  img2 = Math.floor((Math.random() * maxCards)+1);
+  img3 = Math.floor((Math.random() * maxCards)+1);
+  img4 = Math.floor((Math.random() * maxCards)+1);
+  img5 = Math.floor((Math.random() * maxCards)+1);
+  img6 = Math.floor((Math.random() * maxCards)+1);
+  img7 = Math.floor((Math.random() * maxCards)+1);
+  img8 = Math.floor((Math.random() * maxCards)+1);
 
   document.getElementById('card1').src = "cards/" + img1 + ".png";
   document.getElementById('card2').src = "cards/" + img2 + ".png";
@@ -259,6 +383,14 @@ function CountChange()
   }else{
       Count = 0;
       document.getElementById('player').innerHTML = "Black";
+      BricksR += BuildersR;
+      WeaponsR += SoldiersR;
+      CrystalsR += WizardsR;
+
+      BricksB += BuildersB;
+      WeaponsB += SoldiersB;
+      CrystalsB += WizardsB;
+
       }
 
 }
